1173463408d16527d5404992b2e98b74eb7c740d
Erasyl <thetopor600@gamil.com>
2024-04-06 00:46:51 +0600

    fixed logic the cart.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'khanshop', 
     'USER': 'postgres',
-    'PASSWORD': 'Erasil.08',
+    'PASSWORD': 'your password',
     'HOST': '127.0.0.1', 
     'PORT': '5432',
  }
